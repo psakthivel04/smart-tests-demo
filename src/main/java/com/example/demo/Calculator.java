c43ecdd3605797c63b31bf2f782ba2ab97e71ad3
psakthivel <psakthivel@cloudbees.com>
2026-06-12 15:28:24 +0530

    test commit

--- src/main/java/com/example/demo/Calculator.java
+++ src/main/java/com/example/demo/Calculator.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class Calculator {
 
-    public int add(int a, int b) { return a + b; }
+    public int add(int x, int y) { return x + y; }
 
     public int subtract(int a, int b) { return a - b; }
 
