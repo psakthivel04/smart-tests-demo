30077bf53bce775a05b47c38458df7f150d6f784
psakthivel <psakthivel@cloudbees.com>
2026-08-05 14:52:17 +0530

    demo test commit 5

--- src/main/java/com/example/demo/Calculator.java
+++ src/main/java/com/example/demo/Calculator.java
@@ -8,7 +8,6 @@ public class Calculator {
 
     public int multiply(int g, int h) { return g * h; }
 
-
     public int divide(int a, int b) {
         if (b == 0) throw new ArithmeticException("Division by zero");
         return a / b;
